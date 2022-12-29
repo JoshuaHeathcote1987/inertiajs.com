fd8778c177e3bc6ca89cec26a6e7e95b999d2fc4
Joshua <joshuaheathcote1987@gmail.com>
2022-12-29 04:33:05 +0300

    Vue3 isn't working for app.js
    a: resolve: name => require(\`./Pages/\${name}\`),
    
    b: resolve: (name) => import(`./Pages/${name}.vue`),

--- resources/js/Pages/client-side-setup.js
+++ resources/js/Pages/client-side-setup.js
@@ -89,7 +89,7 @@ const Page = () => {
               import { createApp, h } from 'vue'
               import { createInertiaApp } from '@inertiajs/inertia-vue3'\n
               createInertiaApp({
-                resolve: name => require(\`./Pages/\${name}\`),
+                resolve: (name) => import(\`./Pages/\${name}\`),
                 setup({ el, App, props, plugin }) {
                   createApp({ render: () => h(App, props) })
                     .use(plugin)
